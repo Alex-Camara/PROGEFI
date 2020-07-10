04e540ee89bbe1e49ef872371dada8f146f91ad9
Alejandro <arcam_2112@hotmail.com>
2020-07-10 16:51:07 -0500

    se modificaron rutas relativas

--- src/persistence/knexfile.js
+++ src/persistence/knexfile.js
@@ -8,14 +8,17 @@ module.exports = {
     useNullAsDefault: true,
     connection:{
       //filename: path.resolve('.') + '/progefiDB.db'
-    filename: path.resolve('.') + './src/persistence/progefiDB.db'
+      filename: path.join(__dirname, "src", "persistence", "progefiDB.db")
+    // filename: path.resolve('.') + './src/persistence/progefiDB.db'
     },
     migrations: {
-      directory: path.resolve('.') + '/src/persistence/migrations'
+      directory: path.join(__dirname, "src", "persistence", "migrations")
+      // directory: path.resolve('.') + '/src/persistence/migrations'
     },
     seeds: {
       // directory: path.resolve('.') + '/testSeeds'
-      directory: path.resolve('.') + '/src/persistence/productionSeeds'
+      // directory: path.resolve('.') + '/src/persistence/productionSeeds'
+      directory: path.join(__dirname, "src", "persistence", "productionSeeds")
     }
   }
 }
